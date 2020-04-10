09067cc0915a0b71706f4d14efb261bc37ae6aaf
Michael Wurm <wurm.michael95@gmail.com>
2020-04-11 00:38:26 +0200

    clean up

--- ue2/common/StopWatch.h
+++ ue2/common/StopWatch.h
@@ -2,8 +2,26 @@
 #define STOPWATCH_H
 
 namespace stw {
-void Start();
-double Stop();
+
+clock_t StartTime = -1;
+
+void Start() {
+  StartTime = clock();
+}
+
+double Stop() {
+  clock_t Elapsed = 0;
+  clock_t Stop = 0;
+
+  if (StartTime > -1) {
+    Stop = clock();
+    Elapsed = Stop - StartTime;
+    StartTime = -1;
+  }
+
+  return static_cast<double>(Elapsed) / static_cast<double>(CLOCKS_PER_SEC);
+}
+
 }  // namespace stw
 
 #endif /* STOPWATCH_H */
