b31a8748e00d1ee4f8473ec52b75b0b32a10a991
Michael Wurm <wurm.michael95@gmail.com>
2020-04-30 16:56:40 +0200

    wip

---
+++ ue3-7/src/sim/emu_cpu.cpp
@@ -0,0 +1,10 @@
+/*****************************************************************************/
+/**
+ * @file    emu_cpu.cpp
+ * @author  Michael Wurm <michael.wurm@students.fh-hagenberg.at>
+ *          Sebastian Kaupper <sebastian.kaupper@students.fh-hagenberg.at>
+ * @brief   EmuCPU implementation
+ */
+/*****************************************************************************/
+
+#include "emu_cpu.hpp"
---
+++ ue3-7/src/sim/emu_cpu.hpp
@@ -0,0 +1,13 @@
+/*****************************************************************************/
+/**
+ * @file    emu_cpu.hpp
+ * @author  Michael Wurm <michael.wurm@students.fh-hagenberg.at>
+ *          Sebastian Kaupper <sebastian.kaupper@students.fh-hagenberg.at>
+ * @brief   EmuCPU header file
+ */
+/*****************************************************************************/
+
+#ifndef _EMU_CPU_H_
+#define _EMU_CPU_H_
+
+#endif /* _EMU_CPU_H_ */
