db44d79862fb5ec1e851355565c5f5726a5ff2c3
Michael Wurm <wurm.michael95@gmail.com>
2020-05-20 20:18:41 +0200

    revert bd.tcl; clean-up

--- ue4/project/scripts/run.sh
+++ ue4/project/scripts/run.sh
@@ -29,21 +29,10 @@ elif [ "$ARG" == "sdk" ]; then
   xsdk -batch -source open_sdk.tcl
   xsdk -workspace ../sdk #-eclipseargs -perspective org.eclipse.cdt.ui.CPerspective
   exit 0
-elif [ "$ARG" == "format" ]; then
-  folders_to_lint="../sdk/src ../sdk/standalone_bsp_0_Passthrough_A53_1"
-
-  for folder in $folders_to_lint; do
-    for file in $(git ls-files "$folder/*.[ch]"); do
-      echo "Formatting: '$file'"
-      clang-format-6.0 -i $file
-    done
-  done
-  echo "Done formatting all relevant C source and header files."
-  exit 0
 else
   echo "========================================================================"
   echo "(ERROR) Wrong or invalid argument."
-  echo "Usage: ./run.sh <project|synth|sdk|format>"
+  echo "Usage: ./run.sh <project|synth|sdk>"
   echo "========================================================================"
   exit 1
 fi
