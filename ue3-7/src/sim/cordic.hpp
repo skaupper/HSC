960d29195e5b0d2f886fb03483f07255a36d9796
Sebastian Kaupper <kauppersebastian@gmail.com>
2020-05-27 17:52:38 +0200

    fixed point data types

--- ue3-7/src/sim/cordic.hpp
+++ ue3-7/src/sim/cordic.hpp
@@ -11,13 +11,12 @@
 #define _CORDIC_HPP_
 
 #ifndef SC_INCLUDE_FX
-# define SC_INCLUDE_FX // for fixed-point data types (like sc_ufixed)
+#define SC_INCLUDE_FX  // for fixed-point data types (like sc_ufixed)
 #endif
 
 #include <systemc.h>
 
 #include "constants.hpp"
-
 
 
 // 18 iterations to use the maximum resolution of the interface (max. error = 2^-16)
@@ -26,19 +25,18 @@ constexpr int ITERATIONS = 16;
 
 
 constexpr uint8_t PHI_FRAC_BITS = 21;
-constexpr uint8_t PHI_INT_BITS = 1;
-constexpr uint8_t PHI_WL = (PHI_INT_BITS + PHI_FRAC_BITS);
-constexpr uint8_t PHI_IWL = (PHI_INT_BITS);
+constexpr uint8_t PHI_INT_BITS  = 1;
+constexpr uint8_t PHI_WL        = (PHI_INT_BITS + PHI_FRAC_BITS);
+constexpr uint8_t PHI_IWL       = (PHI_INT_BITS);
 
 constexpr uint8_t XY_FRAC_BITS = 16;
-constexpr uint8_t XY_INT_BITS = 0;
-constexpr uint8_t XY_WL = (XY_INT_BITS + XY_FRAC_BITS);
-constexpr uint8_t XY_IWL = (XY_INT_BITS);
+constexpr uint8_t XY_INT_BITS  = 0;
+constexpr uint8_t XY_WL        = (XY_INT_BITS + XY_FRAC_BITS);
+constexpr uint8_t XY_IWL       = (XY_INT_BITS);
 
 
 /* NOTE: The format for ufixed is: ufixed<TOTAL_BITS, INTEGER_BITS>  */
 typedef sc_ufixed<PHI_WL, PHI_IWL, SC_RND, SC_SAT> phi_t;
 typedef sc_ufixed<XY_WL, XY_IWL, SC_RND, SC_SAT> xy_t;
-
 
 #endif /* _CORDIC_HPP_ */
--- ue3-7/src/sim/cordic_cc.cpp
+++ ue3-7/src/sim/cordic_cc.cpp
@@ -12,133 +12,131 @@
 
 using namespace std;
 
-static double customAtan(int i)
-{
-  static double LUT[ITERATIONS];
-  static bool lutInitialized = false;
+static double customAtan(int i) {
+    static double LUT[ITERATIONS];
+    static bool lutInitialized = false;
 
-  if (!lutInitialized) {
-    for(int i = 0; i < ITERATIONS; ++i) {
-      LUT[i] = atan(pow(2, -i));
+    if (!lutInitialized) {
+        for (int i = 0; i < ITERATIONS; ++i) {
+            LUT[i] = atan(pow(2, -i));
+        }
+
+        lutInitialized = true;
     }
 
-    lutInitialized = true;
-  }
-
-  return LUT[i];
+    return LUT[i];
 }
 
-static double precalcK()
-{
-  // Calculates the gain K using the formula K = K / sqrt(1 + pow(2, -2 * i))
-  // For K_0=1 and ITERATIONS iterations
+static double precalcK() {
+    // Calculates the gain K using the formula K = K / sqrt(1 + pow(2, -2 * i))
+    // For K_0=1 and ITERATIONS iterations
 
-  static double K = 1;
-  static bool initialized = false;
+    static double K         = 1;
+    static bool initialized = false;
 
-  if (!initialized) {
-    for (int i = 0; i < ITERATIONS; ++i) {
-      K = K / sqrt(1 + pow(2, -2*i));
+    if (!initialized) {
+        for (int i = 0; i < ITERATIONS; ++i) {
+            K = K / sqrt(1 + pow(2, -2 * i));
+        }
+
+        initialized = true;
     }
 
-    initialized = true;
-  }
-
-  return K;
+    return K;
 }
 
 
-CordicCc::CordicCc(sc_module_name name) : sc_module(name)
-{
-  SC_CTHREAD(calc, iClk.pos());
-  reset_signal_is(inRst, false);
+CordicCc::CordicCc(sc_module_name name) : sc_module(name) {
+    SC_CTHREAD(calc, iClk.pos());
+    reset_signal_is(inRst, false);
 }
 
-void CordicCc::calc()
-{
-  //
-  // Reset routines
-  //
-  oRdy.write(1);
-
-  wait();
-
-
-  //
-  // Variables used
-  //
-
-  // Control state
-  bool calcStarted = false;
-  int currentIter = 0;
-
-  // Constants
-  static const double K = precalcK();
-
-  // Intermediate values
-  phi_t phi;
-  double x;
-  double x_tmp;
-  double y;
-  double z;
-  int d;
-
-
-  while (true)
-  {
-    // If no calculation is started and iStart is low, do nothing.
-    if (iStart == 0 && !calcStarted) {
-      // cout << "Wait for iStart... @" << sc_time_stamp() << endl;
-      wait();
-      continue;
-    }
-
-    // Start a calculation
-    if (iStart == 1 && !calcStarted) {
-      currentIter = 0;
-
-      // Initial values
-      phi   = iPhi.read();
-      x     = 1;
-      x_tmp = 1;
-      y     = 0;
-      z     = phi;
-      d     = 1;
-
-      // Deassert oRdy until the calculation finished
-      oRdy.write(0);
-      calcStarted = true;
-    }
-
-    // cout << "## Start Cordic Calculation Iteration " << currentIter << " @ " << sc_time_stamp() << endl;
-
+void CordicCc::calc() {
     //
-    // Do calculation
+    // Reset routines
     //
-
-    if (z >= 0) {
-      d = 1;
-    } else {
-      d = -1;
-    }
-
-    z = z - d * customAtan(currentIter);
-    x_tmp = x - y * d * pow(2, -currentIter);
-    y = y + x * d * pow(2, -currentIter);
-    x = x_tmp;
-
-
-
-    // Check if the calculation ended
-    if (currentIter == ITERATIONS-1) {
-      oX.write(x * K);
-      oY.write(y * K);
-      oRdy.write(1);
-      calcStarted = false;
-    }
-
-    currentIter++;
+    oRdy.write(1);
 
     wait();
-  }
+
+
+    //
+    // Variables used
+    //
+
+    typedef sc_fixed<28, 2> z_calc_t;
+    typedef sc_fixed<20, 2> xy_calc_t;
+
+
+    // Control state
+    bool calcStarted = false;
+    int currentIter  = 0;  // TODO: does HLS infer a 32 bit counter??
+
+    // Constants
+    static const xy_calc_t K = precalcK();
+
+    // Intermediate values
+    phi_t phi;
+    xy_calc_t x;
+    xy_calc_t x_tmp;
+    xy_calc_t y;
+    z_calc_t z;
+    int d;
+
+
+    while (true) {
+        // If no calculation is started and iStart is low, do nothing.
+        if (iStart == 0 && !calcStarted) {
+            // cout << "Wait for iStart... @" << sc_time_stamp() << endl;
+            wait();
+            continue;
+        }
+
+        // Start a calculation
+        if (iStart == 1 && !calcStarted) {
+            currentIter = 0;
+
+            // Initial values
+            phi   = iPhi.read();
+            x     = 1;
+            x_tmp = 1;
+            y     = 0;
+            z     = phi;
+            d     = 1;
+
+            // Deassert oRdy until the calculation finished
+            oRdy.write(0);
+            calcStarted = true;
+        }
+
+        // cout << "## Start Cordic Calculation Iteration " << currentIter << " @ " << sc_time_stamp() << endl;
+
+        //
+        // Do calculation
+        //
+
+        if (z >= 0) {
+            d = 1;
+        } else {
+            d = -1;
+        }
+
+        z     = z - d * customAtan(currentIter);
+        x_tmp = x - y * d * pow(2, -currentIter);
+        y     = y + x * d * pow(2, -currentIter);
+        x     = x_tmp;
+
+
+        // Check if the calculation ended
+        if (currentIter == ITERATIONS - 1) {
+            oX.write(x * K);
+            oY.write(y * K);
+            oRdy.write(1);
+            calcStarted = false;
+        }
+
+        currentIter++;
+
+        wait();
+    }
 }
