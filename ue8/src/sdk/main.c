51ada92ac6f22130bcbb217b021ae59b7b44cda4
Sebastian Kaupper <kauppersebastian@gmail.com>
2020-07-07 10:59:21 +0200

    remove private timer

--- ue8/src/sdk/main.c
+++ ue8/src/sdk/main.c
@@ -3,20 +3,11 @@
 #include <xil_printf.h>
 
 #include "platform.h"
-#include "private_timer.h"
 
 extern int main_c();
 
 #define msleep(msec) usleep((msec)*1000)
 
-static XScuGic intc;
-
-static void timerExpired(void *state) {
-  XScuTimer *timer = (XScuTimer *)state;
-  XScuTimer_ClearInterruptStatus(timer);
-
-  xil_printf("Timer expired\r\n");
-}
 
 int main() {
   xil_printf("### Main Program running now ... ###\r\n");
@@ -25,10 +16,6 @@ int main() {
 
   // Do initialization
   init_platform();
-
-  status |= PrivateTimer_Init(&intc);
-  PrivateTimer_SetFrequency(2);
-  PrivateTimer_SetIntHandler((Xil_ExceptionHandler)timerExpired);
 
   if (status != XST_SUCCESS) {
     xil_printf("Timer initialization failed!!!\r\n");
